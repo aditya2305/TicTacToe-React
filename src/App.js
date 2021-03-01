dfc6dec2f6833e117732c14cde34f101caf4c60d
Aditya Saxena <aditya.saxena33@gmail.com>
2021-03-01 10:53:20 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 
 import Icon from "./components/Icon";
 
@@ -17,48 +16,72 @@ const App = () => {
   const [winMessage, setWinMessage] = useState("");
 
   const reloadGame = () => {
-    setIsCross(false)
-    setWinMessage("")
-    itemArray.fill("empty",0,9)
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
   };
 
   const checkIsWinner = () => {
-    if ((itemArray[0]===itemArray[1]) && (itemArray[0]===itemArray[2]) && (itemArray[0] !== "empty")) {
-      setWinMessage(`${itemArray[0]} wins`)
-    }
-    else if((itemArray[3]===itemArray[4]) && (itemArray[3]===itemArray[5]) && (itemArray[3] !== "empty")) {
-      setWinMessage(`${itemArray[3]} wins`)
-    }
-    else if((itemArray[6]===itemArray[7]) && (itemArray[6]===itemArray[8]) && (itemArray[6] !== "empty")) {
-      setWinMessage(`${itemArray[6]} wins`)
-    }
-    else if((itemArray[0]===itemArray[3]) && (itemArray[0]===itemArray[6]) && (itemArray[0] !== "empty")) {
-      setWinMessage(`${itemArray[0]} wins`)
-    }
-    else if((itemArray[1]===itemArray[4]) && (itemArray[1]===itemArray[7]) && (itemArray[1] !== "empty")) {
-      setWinMessage(`${itemArray[1]} wins`)
-    }
-    else if((itemArray[2]===itemArray[5]) && (itemArray[2]===itemArray[8]) && (itemArray[2] !== "empty")) {
-      setWinMessage(`${itemArray[2]} wins`)
-    }
-    else if((itemArray[0]===itemArray[4]) && (itemArray[0]===itemArray[8]) && (itemArray[0] !== "empty")) {
-      setWinMessage(`${itemArray[0]} wins`)
-    }
-    else if((itemArray[6]===itemArray[4]) && (itemArray[6]===itemArray[2]) && (itemArray[6] !== "empty")) {
-      setWinMessage(`${itemArray[6]} wins`)
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[3] === itemArray[4] &&
+      itemArray[3] === itemArray[5] &&
+      itemArray[3] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[3]} wins`);
+    } else if (
+      itemArray[6] === itemArray[7] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[6] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (
+      itemArray[0] === itemArray[3] &&
+      itemArray[0] === itemArray[6] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7] &&
+      itemArray[1] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (
+      itemArray[2] === itemArray[5] &&
+      itemArray[2] === itemArray[8] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    } else if (
+      itemArray[0] === itemArray[4] &&
+      itemArray[0] === itemArray[8] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[6] === itemArray[4] &&
+      itemArray[6] === itemArray[2] &&
+      itemArray[6] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
     }
   };
 
-  const changeItem = itemNumber => {
+  const changeItem = (itemNumber) => {
     if (winMessage) {
-      return toast(winMessage,{type: "success"});
+      return toast(winMessage, { type: "success" });
     }
-    if (itemArray[itemNumber]==="empty") {
-      itemArray[itemNumber]= isCross ? "cross" : "circle"
-      setIsCross(!isCross)
-    }
-    else{
-      return toast("Already Filled",{type: "error"});
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("Already Filled", { type: "error" });
     }
 
     checkIsWinner();
@@ -69,31 +92,32 @@ const App = () => {
       <ToastContainer position="bottom-center" />
       <Row>
         <Col id="container" className="offset-md-3 col-md-6">
-        <h1 id="h1" className="text-center">Tic Tac Toe</h1>
-        <div className="grid">
+          <h1 id="h1" className="text-center">
+            Tic Tac Toe
+          </h1>
+          <div className="grid">
             {itemArray.map((item, index) => (
-              <Card id="box" onClick={()=>changeItem(index)}>
+              <Card id="box" onClick={() => changeItem(index)}>
                 <CardBody className="box">
                   <Icon name={item} />
                 </CardBody>
               </Card>
             ))}
           </div>
-        {winMessage ? (
-          <div className="mb-2 mt-2">
-            <h1 className="text-success text-uppercase text-center">
-              {winMessage}
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-success text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>
+                Reload Game
+              </Button>
+            </div>
+          ) : (
+            <h1 id="h2" className="text-center">
+              {isCross ? "Cross's" : "Circle's"} turn
             </h1>
-            <Button color="success" block onClick={reloadGame}>
-            Reload Game
-            </Button>
-          </div>
-        ) : (
-          <h1 id="h2" className="text-center">
-          {isCross ? "Cross's" : "Circle's"} turn
-          </h1>
-        )}
-          
+          )}
         </Col>
       </Row>
     </Container>
